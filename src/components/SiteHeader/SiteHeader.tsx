c1b01f09b14c40d02e3216ec6edb088ae73861b0
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2023-08-20 22:50:19 +0200

    fixed alignment and poisition of top section

--- src/components/SiteHeader/SiteHeader.tsx
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -3,7 +3,13 @@ import "./SiteHeader.less";
 export default function SiteHeader() {
   return (
     <header className="header">
-      <img className="header__logo" src="/assets/images/logo.svg" alt="logo" />
+      <a href="#">
+        <img
+          className="header__logo"
+          src="/assets/images/logo.svg"
+          alt="logo"
+        />
+      </a>
     </header>
   );
 }
